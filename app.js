451ce7238119f5892aa408311e931cefb502cb19
tina-fernan <tinaaroom@gmail.com>
2021-02-04 19:25:30 +0100

    Identify a request

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const session = require('express-session')
 const { use } = require('./router')
 const app = express()
 const router = require('./router')
 
+let sessionOption = session({
+    secret: "JavaScript is sooooooooooooooo cooool",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
+})
+
+app.use(sessionOption)
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,7 @@ exports.login = function(req, res)
 {
     let user = new User(req.body)
     user.login().then(function(result){
+        req.session.user = {favColor: "blue", username: user.data.username}
         res.send(result)
     }).catch(function(err){
 
@@ -32,5 +33,11 @@ exports.register= function(req, res){
 
 
 exports.home = function(req, res){
-    res.render('home-guest')
+
+    if (req.session.user) {
+        res.send("Welcome to the actual application!")
+    } else {
+        
+        res.render('home-guest')
+    }
 }
